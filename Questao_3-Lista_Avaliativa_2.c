7d84fa2c69edea79e9fe32e0c9b28675201ffa48
JeanMoutinho98 <142322746+JeanMoutinho98@users.noreply.github.com>
2023-10-31 23:13:18 -0300

    Add files via upload

--- Questao_3-Lista_Avaliativa_2.c
+++ Questao_3-Lista_Avaliativa_2.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
+#include <stdio.h>                                                  // Se não funcionar no Marvin só pode ser problema de formatação na saída.
 #include <stdlib.h>
 
 int main(){
-    int mat1[4][4], mat2[4][4], mat3[4][4], /*mat4[4][4]*/mat5[4][4];
-    int linhas1, colunas1, linhas2, colunas2, linhas3, colunas3/*,linhas_4, colunas4*/;
+    int mat1[4][4], mat2[4][4], mat_soma[4][4], mat_sub[4][4], mat_mult[4][4];
+    int linhas1, colunas1, linhas2, colunas2, linhas_Soma, colunas_soma, linhas_sub, colunas_sub, linhas_mult, colunas_mult, elementos_mult;
     int soma = 0;
-    int i, j, k;
-    char operacao[12];
-    /*
-    char string_soma[4] = "soma";
-    char subtracao[9] = "subtracao";
-    char multiplicacao[12] = "multiplicacao";
-    */
+    char operacao[10];
 
 
     for(linhas1 = 0; linhas1 < 4; linhas1++){
@@ -24,7 +18,7 @@ int main(){
 
     for(linhas1 = 0; linhas1 < 4; linhas1++){
         for(int colunas1 = 0; colunas1 < 4; colunas1++){
-            printf("%d ", mat1[linhas1][colunas1]);
+            printf("%4d ", mat1[linhas1][colunas1]);
         }
         printf("\n");  
     } 
@@ -40,84 +34,72 @@ int main(){
 
     for(linhas2 = 0; linhas2 < 4; linhas2++){
         for(int colunas2 = 0; colunas2 < 4; colunas2++){
-            printf("%d ", mat2[linhas2][colunas2]);
+            printf("%4d ", mat2[linhas2][colunas2]);
         }
         printf("\n");  
     } 
-
-    fflush(stdin);                                  // Para poder usar o fgets depois de scanf.
+                               
     // Escolhe a operação a ser feita.
+    fflush(stdin);                                                  // Para poder usar o fgets depois de scanf.
     printf("Insira a operacao a ser feita:\n");
-    fgets(operacao, 20, stdin);
-    printf("Operacao escolhida: %s", operacao);
-    /*if(strcmp(operacao, string_soma) == 0){
+    fgets(operacao, sizeof(operacao), stdin);
+    operacao[strcspn(operacao, "\n")] = 0;                          // Remove o \n aparentemente.
+
+    
+    if(strcmp(operacao, "soma") == 0){
         printf("Voce escolheu soma\n");
-        // Puxar soma das matrizes;
+        // Faz a soma das matrizes.
+        printf("Soma das Matrizes:\n");
+        for(linhas_Soma = 0; linhas_Soma < 4; linhas_Soma++){
+            for(colunas_soma = 0; colunas_soma < 4; colunas_soma++){
+                mat_soma[linhas_Soma][colunas_soma] = mat1[linhas_Soma][colunas_soma] + mat2[linhas_Soma][colunas_soma];
+            }
+        }
+        for(linhas_Soma = 0; linhas_Soma < 4; linhas_Soma++){
+            for(colunas_soma = 0; colunas_soma < 4; colunas_soma++){
+                printf("%4d ", mat_soma[linhas_Soma][colunas_soma]);
+            }
+            printf("\n");
+        }
     }
-    else if(strcmp(operacao, subtracao) == 0){
+    else if(strcmp(operacao, "sub") == 0){
         printf("Voce escolheu subtracao\n");
-        // Puxar subtração das matrizes;
+        // Subtrai as Matrizes.
+        printf("Subtracao das Matrizes:\n");
+        for(linhas_sub = 0; linhas_sub < 4; linhas_sub++){
+            for(colunas_sub = 0; colunas_sub < 4; colunas_sub++){
+                mat_sub[linhas_sub][colunas_sub] = mat1[linhas_sub][colunas_sub] - mat2[linhas_sub][colunas_sub];
+            }
+        }
+        for(linhas_sub = 0; linhas_sub < 4; linhas_sub++){
+            for(colunas_sub = 0; colunas_sub < 4; colunas_sub++){
+                printf("%4d ", mat_sub[linhas_sub][colunas_sub]);
+            }
+            printf("\n");
+        }
     }
-    else if(strcmp(operacao, multiplicacao) == 0){
-        printf("Voce escolheu soma\n");
-        // Puxar multiplição das matrizes;
+    else if(strcmp(operacao, "mult") == 0){
+        printf("Voce escolheu multiplicacao\n");
+        // Multiplica as Matrizes. 
+        printf("Multiplica as Matrizes:\n");
+        for(linhas_mult = 0; linhas_mult < 4; linhas_mult++){
+            for(colunas_mult = 0; colunas_mult < 4; colunas_mult++){
+                soma = 0;
+                for(elementos_mult= 0; elementos_mult < 4; elementos_mult++){
+                    soma += mat1[linhas_mult][elementos_mult] * mat2[elementos_mult][colunas_mult];
+                }
+                        mat_mult[linhas_mult][colunas_mult] = soma;
+            }
+        }
+        for(linhas_mult= 0; linhas_mult < 4; linhas_mult++){
+            for(colunas_mult = 0; colunas_mult < 4; colunas_mult++){
+                printf("%4d ", mat_mult[linhas_mult][colunas_mult]);
+            }
+                printf("\n");
+        }
     }
     else{
         printf("Erro!\nPor favor escolha uma operacao valida.");
-    }
-    */
-
-    // Faz a soma das matrizes.
-    printf("Soma das Matrizes:\n");
-    for(linhas3 = 0; linhas3 < 4; linhas3++){
-        for(colunas3 = 0; colunas3 < 4; colunas3++){
-            mat3[linhas3][colunas3] = mat1[linhas3][colunas3] + mat2[linhas3][colunas3];
-        }
-    }
-
-
-    for(linhas3 = 0; linhas3 < 4; linhas3++){
-        for(colunas3 = 0; colunas3 < 4; colunas3++){
-            printf("%d ", mat3[linhas3][colunas3]);
-        }
-        printf("\n");
-    }
-
-
-    /*Subtrai as Matrizes.
-    printf("Subtracao das Matrizes:\n");
-    for(linhas4 = 0; linhas4 < 4; linhas4++){
-        for(linhas4 = 0; linhas4 < 4; linhas4++){
-            mat4[linhas4][linhas4] = mat1[linhas4][linhas4] - mat2[linhas4][linhas4];
-        }
-    }
-
-
-    for(linhas4 = 0; linhas4 < 4; linhas4){
-        for(linhas4 = 0; linhas4 < 4; linhas4++){
-            printf("%d ", mat_Subtracao[linhas4][linhas4]);
-        }
-        printf("\n");
-    }*/
-
-    // Multiplica as Matrizes. 
-    printf("Multiplica as Matrizes:\n");
-    for(i = 0; i < 4; i++){
-        for(j = 0; j < 4; j++){
-            soma = 0;
-           for(k= 0; k < 4; k++){
-            soma += mat1[i][k] * mat2[k][j];
-           }
-           mat5[i][j] = soma;
-        }
-    }
-
-
-    for(i= 0; i < 4; i++){
-        for(j = 0; j < 4; j++){
-            printf("%d ", mat5[i][j]);
-        }
-        printf("\n");
     }
 
 
